b1fe3bd7a7a4207608fbdff8227bd864cd34e5fd
MakDon <379857334@qq.com>
2017-05-24 21:30:33 +0800

    a big update
    更改了查询和插入方法，维护内存一个dict和硬盘一个dict
    加入了权限控制
    加入了插入删除
    issues：有时候控制消息会附带奇怪的空白字符导致分句错误

--- 2.7/loadjson.py
+++ 2.7/loadjson.py
@@ -8,82 +8,159 @@ import codecs
 
 reload(sys)  
 sys.setdefaultencoding('utf8')
-        
+
+    
+#Use the Tuling API to get the reply
 def tuling(content):
-    toPost = { "key" : "your_key_here" , "info" : content,"loc":"your_location","userid":"qqqun"}
+    toPost = { "key" : "YourAPIKey" , "info" : content,"loc":"YourCity","userid":"qqqun"}
     r = requests.post("http://www.tuling123.com/openapi/api", data=toPost)
     replydata = r.json()
     #if r[code] == 100000 : return r[text]
     #else : return r[text]
     return replydata["text"]
 
-def adminhelp():
-    reply = """--tulingoff  关闭图灵机器人(关键字检索仍有效） 
+
+    
+
+    
+
+    
+class Roader:
+    def __init__(self):
+        with io.open("chat.json","r",encoding='utf8') as json_file:
+            self.chatdict = json.load(json_file)
+        with io.open("help.json","r",encoding='utf8') as json_file:
+            self.helpdict = json.load(json_file)
+    
+    #return normal reply
+    def reply(self,content,tulingbot = True):
+        if "--help_all" in content:
+            reply = ''
+            for key in self.helpdict:
+                reply = reply + '--' + key + '\t' + self.helpdict[key] + '\n'
+            return reply 
+        elif "--help" in content:
+            reply = '--help_all\n'
+            for key in self.helpdict:
+                reply = reply + '--' + key + '\n'
+            return reply                       
+        for key in self.helpdict:
+            if key in content:
+                return self.helpdict[key]
+        for key in self.chatdict:
+            if key in content:
+                return self.chatdict[key]
+        if tulingbot :return tuling(content)
+        return None
+    
+    #refresh two dict
+    def refresh(self):
+        with io.open("chat.json","r",encoding='utf8') as json_file:
+            self.chatdict = json.load(json_file)
+        with io.open("help.json","r",encoding='utf8') as json_file:
+            self.helpdict = json.load(json_file)
+        return "refresh done"
+    
+
+    #add rule into chat dict
+    def add_chat(self,content,buddy=False):
+        addflag = True
+        data = {}
+        lines = content.split(' ',4)
+        print lines
+        print lines
+        if (lines[0] == '[@ME]' and lines[2] == '--addchat' and len(lines)==5) or buddy:
+            for key in self.chatdict : 
+                if lines[3]  == key: addflag = False
+            if addflag == True :
+                with codecs.open('chat.json','w','utf-8') as json_filew:
+                    data =self.chatdict
+                    data.update({lines[3]:lines[4]})
+                    self.chatdict = data
+                    data_con=dict([(k.encode('utf-8'), v.encode('utf-8')) for k, v in data.items()])
+                    data_str=json.dumps(data,ensure_ascii=False).encode('utf8')
+                    json_filew.write(data_str)
+                    reply = 'add chat \"'+format(lines[3])+'\" : \"'+format(lines[4])+'\"'
+                    return reply
+            return 'Key already exists'
+        return '语法错误'
+    
+    #add rule into help dict
+    def add_help(self,content,buddy=False):
+        addflag = True
+        data = {}
+        lines = content.split(' ',4)
+        print lines
+        print lines            
+        if (lines[0] == '[@ME]' and lines[2] == '--addhelp' and len(lines)==5) or buddy:
+            for key in self.helpdict : 
+                if lines [3]  == key: addflag = False
+            if addflag == True :
+                with codecs.open('help.json','w','utf-8') as json_filew:
+                    data = self.helpdict
+                    data.update({lines[3]:lines[4]})
+                    self.helpdict = data
+                    data_con=dict([(k.encode('utf-8'), v.encode('utf-8')) for k, v in data.items()])
+                    data_str=json.dumps(data,ensure_ascii=False).encode('utf8')
+                    json_filew.write(data_str)
+                    reply = 'add help \"'+format(lines[3])+'\" : \"'+format(lines[4])+'\"'
+                    return reply
+            return 'Key already exists'
+        return '语法错误'
+    
+    #delete rule from help dict
+    def delete_help(self,content):
+        delete_flag = False
+        keys_ = content.split(' ')
+        keys = keys_[2:]
+        for key in keys:
+            if self.helpdict.has_key(key):
+                del self.helpdict[key]
+                delete_flag = True
+        if delete_flag:
+            with codecs.open('help.json','w','utf-8') as json_filew:
+                data = self.helpdict
+                data_con=dict([(k.encode('utf-8'), v.encode('utf-8')) for k, v in data.items()])
+                data_str=json.dumps(data,ensure_ascii=False).encode('utf8')
+                json_filew.write(data_str)
+                reply = 'deletehelp done'
+                return reply
+        return 'delete nothing'
+    
+    #delete rule from chat dict
+    def delete_chat(self,content):
+        delete_flag = False
+        keys_ = content.split(' ')
+        keys = keys_[2:]
+        for key in keys:
+            if self.chatdict.has_key(key):
+                del self.chatdict[key]
+                delete_flag = True
+        if delete_flag:
+            with codecs.open('chat.json','w','utf-8') as json_filew:
+                data = self.chatdict
+                data_con=dict([(k.encode('utf-8'), v.encode('utf-8')) for k, v in data.items()])
+                data_str=json.dumps(data,ensure_ascii=False).encode('utf8')
+                json_filew.write(data_str)
+                reply = 'deletechat done'
+                return reply
+        return 'delete nothing'
+
+
+    #return the help list for the admin
+    def adminhelp(self):
+        reply = """--tulingoff  关闭图灵机器人(关键字检索仍有效） 
 --tulingon      开启图灵机器人
 --stop              关闭QQ机器人
 --sleep             机器人休眠5分钟
 --on                开启机器人
 --restart           重启机器人
+--addchat key value 增加新的chat项
 --addhelp key value 增加新的help项
 """
-    return reply
-    
-def add_help(content,buddy=False):
-    addflag = True
-    data = {}
-    lines = content.split(' ',4)
-    print lines
-    if (lines[0] == '[@ME]' and lines[2] == '--addhelp' and len(lines)==5) or buddy:
-        with io.open('help.json','r',encoding='utf-8') as json_file:
-            data =json.load(json_file)
-            for key in data : 
-               if lines [3]  == key: addflag = False
-        if addflag == True :
-            with codecs.open('help.json','w','utf-8') as json_filew:
-               data.update({lines[3]:lines[4]})
-               print data
-               data_con=dict([(k.encode('utf-8'), v.encode('utf-8')) for k, v in data.items()])
-               print data_con
-               data_str=json.dumps(data,ensure_ascii=False).encode('utf8')
-               print data_str
-               print type(data_str)
-               json_filew.write(data_str)
-               reply = 'add help \"'+format(lines[3])+'\" : \"'+format(lines[4])+'\"'
-               return reply
-        return 'Key already exists'
-    return '语法错误'
-    
-
-def reply(content,tulingbot = True):
-    #content.encode('utf8')
-#help here ,--help would return the help list
-#===========================================
-    with io.open("help.json","r",encoding='utf8') as json_file:
-        data = json.load(json_file)
-        if "--help" in content:
-            reply = ''
-            for key in data:
-                reply = '--' + key + '\n'
-            return reply
-        for key in data:
-            if key in content:
-                return data[key]
-#chat here,--help would not return the list
-#====================================                
-    with io.open("chat.json","r",encoding='utf8') as json_file:
-        data = json.load(json_file)
-        for key in data:
-            if key in content:
-                return data[key]
-        
-#tuling here
-#=========================================
-    if tulingbot :return tuling(content)
-
-#else
-#========================================
-    return None
-    
+        return reply    
+          
 if __name__ == '__main__':
-    re=add_help('--admin 0 --addhelp key value',True)
+    roader = Roader()
+    re=roader.add_help('--admin 0 --addhelp 雪aaawadsd 也是偶像',True)
     print(re)
--- 2.7/sample1.py
+++ 2.7/sample1.py
@@ -10,98 +10,145 @@ import time
 availtime = time.time()
 bot_avil = True
 tulingbot = True
+contenttype = 0
+roader = loadjson.Roader()
+
+#the Commands
+def commands(bot,contact,member,content,permit,type=99):
+    global contenttype
+    #refresh the help dict and chat dict
+    if '--refresh' in content:
+        if not permit[0]:reply = "permission denied"
+        else : reply = roader.refresh()
+        bot.SendTo(contact,reply)
+        contenttype = type
+        
+    #add rule into chat dict
+    if '--addchat' in content:
+        if not permit[0]:reply = "permission denied"
+        else : reply = roader.add_chat(content)
+        bot.SendTo(contact,reply)
+        contenttype = type
+    
+    #add rule into help dict
+    if '--addhelp' in content:
+        if not permit[1]:reply = "permission denied"
+        else : reply = roader.add_help(content)
+        bot.SendTo(contact,reply)
+        contenttype = type
+    
+    #delete rule from chat dict by key
+    if '--deletechat' in content:
+        if not permit[2]:reply = "permission denied"
+        else : reply = roader.delete_chat(content)
+        bot.SendTo(contact,reply)
+        contenttype = type
+    
+    #delete rule from help dict by key
+    if '--deletehelp' in content:
+        if not permit[3]:reply = "permission denied"
+        else : reply = roader.delete_help(content)
+        bot.SendTo(contact,reply)
+        contenttype = type
+    
+    #show the help list for admin
+    if '--adminhelp' in content:
+        if not permit[4]:reply = "permission denied"
+        else : reply = roader.adminhelp()
+        bot.SendTo(contact,reply)
+        contenttype = type
+    
+    #turn off the Tuling bot(help and chat rules still available
+    if '--tulingoff' in content:
+        if not permit[5]:reply = "permission denied"
+        else : bot.SendTo(contact,'图灵聊天机器人已关闭')
+        tulingbot = False
+        contenttype = type
+    
+    #turn on the Tuling bot
+    if '--tulingon' in content : 
+        if not permit[6]:reply = "permission denied"
+        else : bot.SendTo(contact,'图灵聊天机器人已开启')
+        tulingbot = True
+        contenttype = type
+    
+    #Let the bot temperary unavailable
+    if '--stop' in content : 
+        if not permit[7]:reply = "permission denied"
+        else : bot.SendTo(contact, 'QQ机器人已关闭')
+        bot_avil = False
+        contenttype = type
+        
+    #Let the bot sleep for 5 minutes
+    if '--sleep' in content :
+        if not permit[8]:reply = "permission denied"
+        else : bot.SendTo(contact,'QQ机器人已暂停5分钟')
+        availtime = time.time() + 300
+        contenttype = type
+        
+    #wait up the bot from --stop or --sleep
+    if '--on' in content:
+        if not permit[9]:reply = "permission denied"
+        else : availtime = time.time()
+        bot_avil = True
+        contenttype = type
+    
+    #restart the bot
+    if '--restart' in content :
+        if not permit[10]:reply = "permission denied"
+        else : bot.SendTo(contact,'正在重启')
+        bot.Restart()
+        contenttype = type
+    
+    #kill the qqbot process,if you want to start again you might use the shell
+    if '--kill' in content : 
+        if not permit[11]:reply = "permission denied"
+        else : bot.SendTo(contact, 'QQ机器人已退出')
+        bot.Stop()
+
+
+
+
+
 
 @qqbotslot
 def onQQMessage(bot, contact, member, content):
-    global availtime
-    global bot_avil
-    global tulingbot
-    contenttype = 0
-    #bot.SendTo(contact,content)
+    global availtime#record the time after which the bot can work
+    global bot_avil #record if the bot is avail(record if the bot is not stopped)
+    global tulingbot#record if the TulingBot is on
+    global contenttype#record the content type to avoid a message be processed twice
+    contenttype = 0#reset type every roop
+    
+    #record the permission of the commands
+    permission = (True,True,True,True,True,True,True,True,True,True,True,True)
+    permission2 = (False,False,False,False,False,False,False,False,False,False,False,False)
+    
+    
+    #ban user
+    if contact.ctype == 'group': 
+        if member.qq == '123456789':#QQ number to ban
+            contenttype = 99
+    
     #group admin control
     if contact.ctype == 'group':    
-        if (member.role_id ==0 or member.role_id ==1) and '@ME' in content :
-            if '--addhelp' in content:
-                reply = loadjson.add_help(content)
-                bot.SendTo(contact,reply)
-                contenttype = 3
-            if '--adminhelp' in content:
-                reply = loadjson.adminhelp()
-                bot.SendTo(contact,reply)
-                contenttype = 3
-            if '--tulingoff' in content:
-                bot.SendTo(contact,'图灵聊天机器人已关闭')
-                tulingbot = False
-                contenttype = 3
-            if '--tulingon' in content : 
-                bot.SendTo(contact,'图灵聊天机器人已开启')
-                tulingbot = True
-                contenttype = 3
-            if '--stop' in content : 
-                bot.SendTo(contact, 'QQ机器人已关闭')
-                bot_avil = False
-                contenttype = 3
-            if '--sleep' in content :
-                bot.SendTo(contact,'QQ机器人已暂停5分钟')
-                availtime = time.time() + 300
-                contenttype = 3
-            if '--on' in content:
-                availtime = time.time()
-                bot_avil = True
-                contenttype = 3
-            if '--restart' in content :
-                bot.SendTo(contact,'正在重启')
-                bot.Restart()
-                contenttype = 3
-            if '--kill' in content : 
-                bot.SendTo(contact, 'QQ机器人已退出')
-                bot.Stop()
+        if (member.role_id ==0 or member.role_id ==1) and '@ME' in content and contenttype < 4:
+            commands(bot, contact, member, content,permission,type=3)
+    
+    #group normal members control
+    if  contact.ctype == 'group' and '@ME' in content and member.role_id ==2 and contenttype <3: 
+        commands(bot, contact, member, content,permission2,type=2)
+    
     
     #buddy control
     if contact.ctype == 'buddy' and '--admin' in content and contenttype < 3 :
-        if '--addhelp' in content:
-            reply = loadjson.add_help(content,buddy=True)
-            bot.SendTo(contact,reply)
-            contenttype = 2
-        if '--adminhelp' in content:
-            reply = loadjson.adminhelp()
-            bot.SendTo(contact,reply)
-            contenttype = 2
-        if '--tulingoff' in content:
-            bot.SendTo(contact,'图灵聊天机器人已关闭')
-            tulingbot = False
-            contenttype = 2
-        if '--tulingon' in content : 
-            bot.SendTo(contact,'图灵聊天机器人已开启')
-            tulingbot = True
-            contenttype = 2
-        if '--stop' in content : 
-            bot.SendTo(contact, 'QQ机器人已关闭')
-            bot_avil = False
-            contenttype = 2
-        if '--sleep' in content :
-            bot.SendTo(contact,'QQ机器人已暂停5分钟')
-            availtime = time.time() + 300
-            contenttype = 2
-        if '--on' in content:
-            bot.SendTo(contact,'emmmmmmmm可以回来扯蛋了')
-            availtime = time.time()
-            bot_avil = True
-            contenttype = 2
-        if '--restart' in content :
-            bot.SendTo(contact,'正在重启')
-            bot.Restart()
-            contenttype = 2
-        if '--kill' in content : 
-            bot.SendTo(contact, 'QQ机器人已退出')
-            bot.Stop()
-            
+        commands(bot, contact, member, content,permission,type=2)
+    
     #normal reply        
     if contact.ctype == 'buddy' or (contact.ctype == 'group' and '@ME' in content): 
         if time.time() > availtime and bot_avil and contenttype <2 :
-            reply = loadjson.reply(content,tulingbot)
+            reply = roader.reply(content,tulingbot)
             if reply is not None:
-                if contact.ctype == 'group':reply = '@'+member.name +" " + reply                
+                if contact.ctype == 'group':reply = ' @'+member.name +" " + reply                
                 bot.SendTo(contact,reply)
             
         
